ea011458022d2c01e2d0d615c35e6aa4abba726d
xoxxi <gimm97683@gmail.com>
2024-05-09 23:13:18 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,8 +13,17 @@ function login(){
         id: id.value,
         psword: psword.value
     };
-}
 
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type" : "application/json" //내가 보내는 데이터의 타입을 알려줌
+        }, //내가 전달하는 데이터가 json 데이터라는 것을 알려주어야 함
+        body: JSON.stringify(req), //문자열로 감쌀 수 있음
+
+    })
+}
+//오브젝트로 전달할 떄는 : 씀
 console.log(id);
 console.log("heelo");
 
