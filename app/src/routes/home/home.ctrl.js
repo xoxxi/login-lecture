684224752bfafb2e2677804f6d097d33b3269e44
xoxxi <gimm97683@gmail.com>
2024-05-24 00:06:26 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,8 @@
 "use strict";
 
+const { response } = require("express");
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) =>{
@@ -16,25 +18,29 @@ const output = {
 
 const process = {
     login: (req, res) =>{
-        const id = req.body.id;
-        const password = req.body.password;
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        //return res.json(response);
+        // const id = req.body.id;
+        // const password = req.body.password;
 
-        //const UserStorage = new UserStorage();
+        // //const UserStorage = new UserStorage();
 
-        const  users = UserStorage.getUsers("id", "password");
-        const response = {};
+        // const  users = UserStorage.getUsers("id", "password");
+        // const response = {};
         
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password){
-                response.success = true;
-                return res.json(response);
-            }
-        }
+        // if(users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id);
+        //     if(users.password[idx] === password){
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
 
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response);
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다."
+        // return res.json(response);
     },
 };
 
